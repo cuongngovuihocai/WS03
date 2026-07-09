75b8f307268bd96a4064844cb2f3a88757b8c688
cuongngovuihocai <cuongngovuihocai@gmail.com>
2026-07-09 16:15:45 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/WS03/',
+    base: 'WS03',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
